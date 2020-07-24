c36f1250e67ba7d837fc2599cd04277ca8c9adb1
tyu-ru <tyuru.tw.cpp@gmail.com>
2020-07-24 17:53:05 +0900

    feat: union_find unite

--- src/unionfind.rs
+++ src/unionfind.rs
@@ -13,7 +13,7 @@ impl UnionFind {
         }
     }
 
-    fn root(&mut self, i: usize) -> usize {
+    pub fn root(&mut self, i: usize) -> usize {
         if self.node[i].parent == i {
             return i;
         }
@@ -21,11 +21,16 @@ impl UnionFind {
         self.node[i].parent = r;
         r
     }
-    pub fn unite(&mut self, i: usize, j: usize) {
+    pub fn unite(&mut self, i: usize, j: usize) -> bool {
         let i = self.root(i);
         let j = self.root(j);
-        self.node[i].parent = j;
-        self.node[j].size += std::mem::replace(&mut self.node[i].size, 0);
+        if i == j {
+            false
+        } else {
+            self.node[i].parent = j;
+            self.node[j].size += std::mem::replace(&mut self.node[i].size, 0);
+            true
+        }
     }
     pub fn size(&mut self, i: usize) -> usize {
         let r = self.root(i);
@@ -40,13 +45,13 @@ impl UnionFind {
 fn test_unionfind() {
     let mut uf = UnionFind::new(5);
     assert_eq!(uf.size(0), 1);
-    uf.unite(0, 1);
+    assert_eq!(uf.unite(0, 1), true);
     assert_eq!(uf.is_same(0, 1), true);
     assert_eq!(uf.size(0), 2);
     assert_eq!(uf.size(1), 2);
-    uf.unite(1, 2);
+    assert_eq!(uf.unite(1, 2), true);
     assert_eq!(uf.is_same(0, 2), true);
     assert_eq!(uf.size(2), 3);
-    uf.unite(0, 0);
+    assert_eq!(uf.unite(0, 0), false);
     assert_eq!(uf.size(0), 3);
 }
