0b86584317b33738ae9abebd28983ed291d4c5f6
tyu-ru <tyuru.tw.cpp@gmail.com>
2020-12-28 23:47:32 +0900

    feat: UnionFindWithCS

--- src/unionfind.rs
+++ src/unionfind.rs
@@ -29,15 +29,15 @@ impl UnionFind {
 
     pub fn root(&mut self, i: usize) -> usize {
         self.root_impl(i).idx
-        }
+    }
     fn root_impl(&mut self, i: usize) -> RootInfo {
         match self.node[i] {
             Node::Root(s) => RootInfo { idx: i, size: s },
             Node::Leaf(Parent(p)) => {
                 let r = self.root_impl(p);
                 self.node[i] = Node::Leaf(Parent(r.idx));
-        r
-    }
+                r
+            }
         }
     }
     pub fn unite(&mut self, u: usize, v: usize) -> bool {
@@ -51,8 +51,8 @@ impl UnionFind {
         }
         self.node[u.idx] = Node::Root(Size(u.size.0 + v.size.0));
         self.node[v.idx] = Node::Leaf(Parent(u.idx));
-            true
-        }
+        true
+    }
     pub fn size(&mut self, i: usize) -> usize {
         self.root_impl(i).size.0
     }
@@ -75,4 +75,54 @@ fn test_unionfind() {
     assert_eq!(uf.size(2), 3);
     assert_eq!(uf.unite(0, 0), false);
     assert_eq!(uf.size(0), 3);
+}
+
+pub struct UnionFindWithCS<T, F> {
+    uf: UnionFind,
+    val: Vec<Option<T>>,
+    f: F,
+}
+
+impl<T, F> UnionFindWithCS<T, F>
+where
+    F: Fn(T, T) -> T,
+{
+    pub fn new<F2: Fn(usize) -> T>(n: usize, g: F2, f: F) -> Self {
+        Self {
+            uf: UnionFind::new(n),
+            val: (0..n).map(|i| Some(g(i))).collect(),
+            f: f,
+        }
+    }
+
+    pub fn root(&mut self, i: usize) -> usize {
+        self.uf.root(i)
+    }
+    pub fn unite(&mut self, u: usize, v: usize) -> bool {
+        let mut u = self.uf.root_impl(u);
+        let mut v = self.uf.root_impl(v);
+        if u.idx == v.idx {
+            return false;
+        }
+        if u.size < v.size {
+            std::mem::swap(&mut u, &mut v);
+        }
+        self.uf.node[v.idx] = Node::Leaf(Parent(u.idx));
+        self.uf.node[u.idx] = Node::Root(Size(u.size.0 + v.size.0));
+        self.val[u.idx] = Some((self.f)(
+            self.val[u.idx].take().unwrap(),
+            self.val[v.idx].take().unwrap(),
+        ));
+        true
+    }
+    pub fn size(&mut self, i: usize) -> usize {
+        self.uf.root_impl(i).size.0
+    }
+    pub fn is_same(&mut self, i: usize, j: usize) -> bool {
+        self.root(i) == self.root(j)
+    }
+
+    pub fn value(&mut self, i: usize) -> &mut T {
+        self.val[self.uf.root(i)].as_mut().unwrap()
+    }
 }
