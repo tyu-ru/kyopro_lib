62290851db6e59e05f6c35bb79077d454ecb1b66
tyu-ru <tyuru.tw.cpp@gmail.com>
2021-01-27 13:52:51 +0900

    test: cargo-equip test

---
+++ tests/equip_test/src/lib.rs
@@ -0,0 +1,44 @@
+#[test]
+fn test_equip_remove_tests() {
+    use std::process::{Command, Stdio};
+    if !Command::new("cargo")
+        .arg("equip")
+        .arg("-V")
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .status()
+        .expect("failed to execute process")
+        .success()
+    {
+        // system didn't install cargo-equip
+        return;
+    }
+    let e = std::process::Command::new("cargo")
+        .arg("equip")
+        .arg("--resolve-cfgs")
+        .arg("--remove")
+        .arg("docs")
+        .arg("comments")
+        .arg("--rustfmt")
+        .arg("--exclude-atcoder-crates")
+        .arg("--check")
+        .current_dir(env!("CARGO_MANIFEST_DIR"))
+        .output()
+        .expect("failed to execute process");
+
+    assert_eq!(e.status.success(), true);
+    use itertools::Itertools;
+    let s = std::str::from_utf8(&e.stdout).expect("failed to execute process");
+    for (i, (l1, l2)) in s.lines().tuple_windows().enumerate() {
+        if l1.find("#[test]") != None {
+            let msg = format!(
+                "find `#[test]` \n >>> {}:  {}\n >>> {}:  {}",
+                i + 1,
+                l1,
+                i + 2,
+                l2
+            );
+            Option::<()>::None.expect(&msg);
+        }
+    }
+}
---
+++ tests/equip_test/src/main.rs
@@ -0,0 +1,28 @@
+fn main() {
+    #[allow(unused_imports)]
+    use klalgebra;
+    #[allow(unused_imports)]
+    use klfewnick_tree;
+    #[allow(unused_imports)]
+    use klgrid;
+    #[allow(unused_imports)]
+    use kliterator;
+    #[allow(unused_imports)]
+    use klkitamasa;
+    #[allow(unused_imports)]
+    use klmath;
+    #[allow(unused_imports)]
+    use klmisc;
+    #[allow(unused_imports)]
+    use klmodint;
+    #[allow(unused_imports)]
+    use klrle;
+    #[allow(unused_imports)]
+    use klsegtree;
+    #[allow(unused_imports)]
+    use klsieve;
+    #[allow(unused_imports)]
+    use klstring;
+    #[allow(unused_imports)]
+    use klunionfind;
+}
