2fd1da4287762772cb19eb80a7324840033fe049
tyu-ru <tyuru.tw.cpp@gmail.com>
2021-02-27 11:11:09 +0900

    feat: Add chmin/chmax varg & updated

--- klmacro/src/lib.rs
+++ klmacro/src/lib.rs
@@ -1,12 +1,47 @@
 #[macro_export]
 macro_rules! chmin {
     ($x:expr, $y: expr) => {
-        $x = std::cmp::min($x, $y);
+        if $y < $x {
+            $x = $y;
+            true
+        } else {
+            false
+        }
+    };
+    ($x:expr, $($y:expr),+) => {
+        $(chmin!($x, $y)) || *
     };
 }
+
 #[macro_export]
 macro_rules! chmax {
     ($x:expr, $y: expr) => {
-        $x = std::cmp::max($x, $y);
+        if $y > $x {
+            $x = $y;
+            true
+        } else {
+            false
+        }
     };
+    ($x:expr, $($y:expr),+) => {
+        $(chmax!($x, $y)) || *
+    };
+}
+
+#[cfg(test)]
+#[test]
+fn test() {
+    let mut x = 2;
+    assert_eq!(chmin!(x, 4), false);
+    assert_eq!(x, 2);
+    assert_eq!(chmin!(x, 1), true);
+    assert_eq!(x, 1);
+    assert_eq!(chmin!(x, 1), false);
+    assert_eq!(x, 1);
+
+    let mut y = 4;
+    assert_eq!(chmax!(y, 4, 1, 2, 3), false);
+    assert_eq!(y, 4);
+    assert_eq!(chmax!(y, 4, 1, 6, 5, 2, 3), true);
+    assert_eq!(y, 6);
 }
